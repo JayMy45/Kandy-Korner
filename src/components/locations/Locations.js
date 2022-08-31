03b2ac68c6085c29dabdab38ebeb20886369a954
Jeremy Myers <echelonical@gmail.com>
2022-08-31 11:33:01 -0500

    most of saved progress had been reverted to origin boilerplate code.  Now saved progress...

---
+++ src/components/locations/Locations.js
@@ -0,0 +1,23 @@
+//display a list of location that can be seen by both customers and employees...
+//add navbar item that both kind of users can click on
+//when clicked, display a list of locations. Display the address and sqft. (using any layout and color desired)
+
+/*
+    1. Set initial state of app
+    2. observe the initial state of the app
+    3. create jsx code to iterate and display locations
+*/
+
+
+//declare export function...
+export const locationList = () => {
+    //set initial state
+    const [locations, setLocations] = setState([])
+
+    return <>
+        {<h1>Hello World</h1>}
+    </>
+
+}
+
+//reroute locations to webpage...?
