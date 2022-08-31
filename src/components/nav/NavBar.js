79124a2dac3869d06a2db489b2f66f77f3796d56
Jeremy Myers <echelonical@gmail.com>
2022-08-31 11:28:33 -0500

    updated navBar for link...initial try to push navBar on a different branch didnt work

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -4,12 +4,13 @@ import "./NavBar.css"
 export const NavBar = () => {
     const navigate = useNavigate()
 
+
     return (
         <ul className="navbar">
             <li className="navbar__item navbar__logout">
                 <Link className="navbar__link" to="" onClick={() => {
                     localStorage.removeItem("kandy_user")
-                    navigate("/", {replace: true})
+                    navigate("/", { replace: true })
                 }}>Logout</Link>
             </li>
         </ul>
