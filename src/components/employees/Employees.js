ba6df04e53b3a2d8ec31bb4deeb17a67ee8a1941
Jeremy Myers <echelonical@gmail.com>
2022-09-07 22:51:52 -0500

    created format of forms with necessary input field and dropdowns

--- src/components/employees/Employees.js
+++ src/components/employees/Employees.js
@@ -1,5 +1,7 @@
 //creating a prop from EmployeeList to create jsx code while the information is sent from EmployeeLists
 
+import { Link } from "react-router-dom"
+
 //declare export function to return jsx code.
 //parameter are the props (aka properties) deconstructed into object. From EmployeeList.js 
 export const Employees = ({ fullName, id, email, startDate }) => {
@@ -9,7 +11,9 @@ export const Employees = ({ fullName, id, email, startDate }) => {
         {
 
             <section className="employee" key={`employee--${id}`}>
-                <div><strong>Name:</strong> {fullName}</div>
+                <div className="employee__fullName">
+                    <Link to={`/employees/${id}`}>{fullName}</Link>
+                </div>
                 <div><strong>Email:</strong> {email}</div>
                 <div><strong>start Date:</strong>{startDate}</div>
             </section>
