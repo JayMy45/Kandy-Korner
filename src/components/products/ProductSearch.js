5cafc311c56331ffebb2a59d78fb778d40d69ccd
Jeremy Myers <echelonical@gmail.com>
2023-07-18 11:51:46 -0500

    update to search styling

--- src/components/products/ProductSearch.js
+++ src/components/products/ProductSearch.js
@@ -5,13 +5,20 @@ export const ProductSearch = ({ setterFunction }) => {
         //create an onChange event that will change the state in this component
         //changeEvent.target.value eventListener that will listen for changes to input field value.
 
-        <div>
-            <input
-                onChange={(changeEvent) => { setterFunction(changeEvent.target.value) }}
-                type="text"
-                placeholder="What candy are you looking for?"
-                className="kandyKorner__search"
-            />
+        <div className="searchbox">
+            <div className="search">
+                <div className="searchlabel">
+                    <label>Search for a candy by name!</label>
+                </div>
+                <div className="searchinput">
+                    <input
+                        onChange={(changeEvent) => { setterFunction(changeEvent.target.value) }}
+                        type="text"
+                        placeholder="What candy are you looking for?"
+                        className="kandyKorner__search"
+                    />
+                </div>
+            </div>
         </div>
     )
 } 
